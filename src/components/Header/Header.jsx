173fe138a824df25faf30687ad0542c7db70bf50
VaflyaArtek <yuriy.shpak.2017@gmail.com>
2024-04-09 23:39:15 +0300

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { sendTgId } from '../../hooks/api';
-import {useTelegram} from "../../hooks/useTelegram";
+import { useTelegram } from "../../hooks/useTelegram";
 
 const Header = () => {
     const [userData, setUserData] = useState(null);
     const [error, setError] = useState('');
-    const {tg} = useTelegram()
+    const { tg } = useTelegram();
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                // const response = await sendTgId({ tgId: 544362566 });
-                const response = await sendTgId({ tgId: tg?.initDataUnsafe?.user?.id ||  444555666});
-                setUserData(response.data);
-                setError('');
+                const tgId = tg?.initDataUnsafe?.user?.id || 444555666;
+                const response = await sendTgId({ tgId });
+
+                if (response && response.data && response.data.coins !== undefined) {
+                    setUserData(response.data);
+                    setError('');
+                } else {
+                    throw new Error('Invalid user data received');
+                }
             } catch (error) {
                 setError('Error fetching user data');
                 console.error('Error fetching user data:', error);
@@ -21,7 +26,7 @@ const Header = () => {
         };
 
         fetchData();
-    }, []);
+    }, [tg]);
 
     return (
         <div>
