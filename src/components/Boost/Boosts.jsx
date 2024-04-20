df8e2d1367fb03c4bf4391c51881f641c261e005
VaflyaArtek <yuriy.shpak.2017@gmail.com>
2024-04-20 19:50:47 +0300

    test4

--- src/components/Boost/Boosts.jsx
+++ src/components/Boost/Boosts.jsx
@@ -63,10 +63,10 @@ const Boost = () => {
         try {
             const tgId = tg?.initDataUnsafe?.user?.id || 544362566;
             if (operationType === 'multitap') {
-                window.location.reload();
+                // window.location.reload();
                 await boostUpdate({ tgId: tgId, boost: userBoost + 1 });
             } else if (operationType === 'energyLimit') {
-                window.location.reload();
+                // window.location.reload();
                 await boostUpdate({ tgId: tgId, staminaLimit: userStaminaLimit + 1000 });
             }
         } catch (error) {
@@ -119,7 +119,7 @@ const Boost = () => {
                             <div>
                                 <span>{operationPrice}</span><img className={'popup-coin'} src={'./media/coins-solid.svg'} alt={'coin'} />
                             </div>
-                            {/*<button onClick={handleConfirm} className={'modal-btn'}>Confirm</button>*/}
+                            <button onClick={handleConfirm} className={'modal-btn'}>Confirm</button>
                         </div>
                     </div>
                 </div>
